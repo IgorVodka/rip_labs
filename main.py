3d0e3ad1d88dcf62fe1dbdd8647c590b92d3f083
igor-vodka <mail@igor.vodka>
2018-09-20 21:57:47 +0300

    Make lab

---
+++ main.py
@@ -0,0 +1,17 @@
+from lab_python_oop.shapes import Rectangle, Circle, Square, Color
+
+
+def main():
+    rectangle = Rectangle(3, 2, Color('синий'))
+    circle = Circle(5, Color('зеленый'))
+    square = Square(5, Color('красный'))
+
+    shapes = [rectangle, circle, square]
+    result = ', '.join(str(shape) for shape in shapes)
+
+    print("У меня есть:", result)
+
+
+if __name__ == '__main__':
+    # execute only if run as a script
+    main()
